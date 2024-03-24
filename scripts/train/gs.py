9bbce2b936b2ddb65f66c0bc9a58ff0030214d51
Mahdi Nikdan <nikdanmahdi@gmail.com>
2024-03-24 16:35:17 +0100

    Update gs.py

--- scripts/train/gs.py
+++ scripts/train/gs.py
@@ -7,6 +7,7 @@ def upload_from_directory(directory_path: str, dest_bucket_name: str, dest_blob_
     rel_paths = glob.glob(directory_path + '/**', recursive=True)
     bucket = GCS_CLIENT.get_bucket(dest_bucket_name)
     for local_file in rel_paths:
+        print(local_file)
         remote_path = f'{dest_blob_name}/{"/".join(local_file.split(os.sep)[1:])}'
         if os.path.isfile(local_file):
             blob = bucket.blob(remote_path)
