009cd35aa9790f83b11022cae2ef1e0e33aaf4ef
Mahdi NIKDAN <mahdi.nikdan@ist.ac.at>
2024-04-20 13:53:50 +0200

    gs

--- scripts/train/gs.py
+++ scripts/train/gs.py
@@ -3,17 +3,23 @@ import os
 from google.cloud import storage
 from pathlib import Path
 
-def download_directory(bucket_name, prefix, dl_dir):
+def download_directory(bucket_name, prefix):
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name=bucket_name)
+    bucket = storage_client.get_bucket(bucket_name)
     blobs = bucket.list_blobs(prefix=prefix)  # Get list of files
+    prefix = prefix.rstrip("/")
+    dir_name = prefix.split("/")[-1]
+    print(f"dir_name: {dir_name}")
     for blob in blobs:
-        if blob.name.endswith("/"):
+        breakpoint()
+        blob_name = blob.name.replace(prefix + '/', dir_name + '/')
+        print(blob.name, blob_name)
+        if blob_name.endswith("/"):
             continue
-        file_split = blob.name.split("/")
+        file_split = blob_name.split("/")
         directory = "/".join(file_split[0:-1])
         Path(directory).mkdir(parents=True, exist_ok=True)
-        blob.download_to_filename(blob.name)
+        blob.download_to_filename(blob_name)
 
 def upload_from_directory(directory_path: str, dest_bucket_name: str, dest_blob_name: str):
     GCS_CLIENT = storage.Client()
